22b096415c2d5dfbe885df6f0573ecbc63275944
Hedrs <hedrs@yandex.ru>
2025-06-16 18:17:54 +0300

    Test 1.2

--- frontend/src/pages/index.page.tsx
+++ frontend/src/pages/index.page.tsx
@@ -39,7 +39,7 @@ function Page() {
     <>
       <div className={styles.wrapper}>
         <div className={styles.container}>
-          <p>Test</p>
+          <p className="text-7xl">Test</p>
           {/* {isMobile || isTablet || isSmallMobile ? (
             <>
               <div>
