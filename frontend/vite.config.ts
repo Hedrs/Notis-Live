0bc33c21735528ac4efa1af5ae5227acc0c09c6f
Hedrs <hedrs@yandex.ru>
2025-06-17 20:17:15 +0300

    Test 1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vite';
+import { build, defineConfig, optimizeDeps } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import vike from 'vike/plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['react', 'react-dom'], // Предварительная оптимизация зависимостей
+  },
+  build: {
+    minify: 'esbuild', // Используем esbuild для минимизации
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('/pages/')) {
+            return 'pages';
+          }
+        },
+      },
+    },
+  },
   plugins: [tailwindcss(), react(), vike({ prerender: true })],
 });
